e07938a1a005cc5e5ca845254efc9a8191e572e9
eroko <cloverqi@gmail.com>
2020-08-25 07:05:40 +0000

    Fix dd

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+
 class PagesController extends Controller
 {
     public function root()
     {
-        dd(\Auth::user()->hasVerifiedEmail());
+        //dd(\Auth::user()->hasVerifiedEmail());
+
         return view('pages.root');
     }
 }
