b3d38a94dfd82e12b6a9b0b006f7460955fb66ea
eroko <cloverqi@gmail.com>
2020-08-26 01:24:40 +0000

    Crop Oversized Avatar

--- app/Handlers/ImageUploadHandler.php
+++ app/Handlers/ImageUploadHandler.php
@@ -8,7 +8,7 @@ class ImageUploadHandler
 {
     protected $allowed_ext = ['jpg', 'gif', 'png', 'jpeg'];
 
-    public function save($file, $folder, $file_prefix)
+    public function save($file, $folder, $file_prefix, $max_width = false)
     {
         $folder_name = "uploads/images/$folder/" . date('Ym/d', time());
 
@@ -24,8 +24,26 @@ class ImageUploadHandler
 
         $file->move($upload_path, $file_name);
 
+        // Crop Oversized Image
+        if ($max_width && $extension != 'gif') {
+            $this->reduceSize($upload_path . '/' . $file_name, $max_width);
+        }
+
         return [
             'path' => config('app.url') . "/$folder_name/$file_name"
         ];
+    }
+
+    public function reduceSize($file_path, $max_width)
+    {
+        $image = Image::make($file_path);
+
+        $image->resize($max_width, null, function ($constraint) {
+            $constraint->aspectRatio();
+
+            $constraint->upsize();
+        });
+
+        $image->save();
     }
 }
